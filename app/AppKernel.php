bc5a0094d7b530a130d4f4e25c47c733c7a0f72a
Edwar Alejandro Giraldo <alejo491@gmail.com>
2016-10-20 07:48:51 -0500

    eliminar fosuser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,6 @@ class AppKernel extends Kernel
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Dizda\OnesignalApiBundle\DizdaOnesignalApiBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
